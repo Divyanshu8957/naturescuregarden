de55c463622b2b764c76ecb1ee9877f721223f6b
DIVYANSHU KUSHWAHA <160529610+Divyanshu4381@users.noreply.github.com>
2025-03-21 18:02:33 +0530

    Fixed line endings and updated project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Default 500KB, इसे 2000KB (2MB) कर रहे हैं
+  },
 })
